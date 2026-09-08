da2042e6ed3d4533eeece0bb082f800ec2eaf9f0
Benjamin Chizorom <bmaurice367@gmail.com>
2026-09-08 21:22:06 +0100

    fix some bugs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,32 +17,25 @@ const NavBar = ({ setPage }) => {
         </div> */}
 
         <div className="footer-logo">
-             <h5 id="logo">🔴REC</h5>
+          <h5 id="logo">🔴REC</h5>
 
-             <div className="footer-title">
-                 <h5>O147</h5>
-              <p id="rec">RECORDS</p>
-             </div>
-
+          <div className="footer-title" onClick={() => setPage("home")}>
+            <h5>O147</h5>
+            <p id="rec">RECORDS</p>
           </div>
+        </div>
 
         <div className="navbar-link">
           <h5 onClick={() => setPage("home")}>HOME</h5>
-          <h5 onClick={() => setPage("about")}>ABOUT</h5> 
-          <h5 onClick={() => setPage("a&r development")}>
-          A&R / DEVELOPMENT
-          </h5>
-          
+          <h5 onClick={() => setPage("about")}>ABOUT</h5>
+          <h5 onClick={() => setPage("a&r development")}>A&R / DEVELOPMENT</h5>
+
           <h5 onClick={() => setPage("news")}>NEWS</h5>
-          
-          <h5 onClick={() => setPage("contact")}>CONTACT</h5> 
 
-          
+          <h5 onClick={() => setPage("contact")}>CONTACT</h5>
         </div>
-        
+
         <button onClick={() => setPage("submit")}>SUBMIT YOUR MUSIC</button>
-
-
       </div>
     </>
   );
